acbcf48e8714cdb6876bb7614c1aac568865acf8
Arron Zhu <1274294199@qq.com>
2025-07-26 18:26:41 +0800

    调整部署

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
  * @Author: arron Zhu
  * @Date: 2025-07-26 17:31:45
  * @lastEditor: arron Zhu
- * @LastEditTime: 2025-07-26 18:17:49
+ * @LastEditTime: 2025-07-26 18:19:51
  * @Description: 
 -->
 <template>
   <el-container>
   <el-row :gutter="20" style="width: 100%; height: 100vh;">
-    <el-col :span="6" :offset="0"></el-col>
+    <el-col :span="6" :offset="0">
+    
+    </el-col>
     <el-col :span="18" :offset="0">
       <el-header>
         <FilterPanel />
